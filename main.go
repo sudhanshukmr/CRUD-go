8704b42aed5840760cff8afd8204ea6e29f4a49e
Sudhanshu Kumar <sudhanshukmrrock@gmail.com>
2022-07-22 13:25:48 +0530

    unwanted comments removed

--- main.go
+++ main.go
@@ -4,108 +4,14 @@ import (
 	"Restapi/controllers"
 	"Restapi/database"
 	"Restapi/entity"
+	"fmt"
 	"log"
 	"net/http"
-
-	// "net/http"
-	"fmt"
 
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-// var profiles []entity.Profile = []entity.Profile{}
-
-/*
-// type User struct {
-// 	FirstName string `json:"firstname"`
-// 	LastName  string `json:"lastname"`
-// 	Email     string `json:"email"`
-// }
-// type Profile struct {
-// 	Department  string `json:"department"`
-// 	Designation string `json:"designation"`
-// 	Employee    User   `json:"employee"`
-// }
-
-func addItem(q http.ResponseWriter, r *http.Request) {
-	var newProfile entity.Profile
-	json.NewDecoder(r.Body).Decode(&newProfile)
-
-	q.Header().Set("Content-Type", "application/json")
-
-	profiles = append(profiles, newProfile)
-
-	json.NewEncoder(q).Encode(profiles)
-}
-
-func getAllProfiles(q http.ResponseWriter, r *http.Request) {
-	q.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(q).Encode(profiles)
-}
-
-func getProfiles(q http.ResponseWriter, r *http.Request) {
-
-	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		q.WriteHeader(http.StatusBadRequest)
-		q.Write([]byte("Id could not be converted to integer"))
-		return
-	}
-	if id >= len(profiles) {
-		q.WriteHeader(http.StatusNotFound)
-		q.Write([]byte("no profile found with specific ID"))
-		return
-	}
-	profile := profiles[id]
-	q.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(q).Encode(profile)
-}
-
-func updateProfiles(q http.ResponseWriter, r *http.Request) {
-
-	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		q.WriteHeader(400)
-		q.Write([]byte("Id could not be converted to integer"))
-		return
-	}
-	if id >= len(profiles) {
-		q.WriteHeader(404)
-		q.Write([]byte("no profile found with specific ID"))
-		return
-	}
-	var updateProfiles entity.Profile
-	json.NewDecoder(r.Body).Decode(&updateProfiles)
-	profiles[id] = updateProfiles
-	q.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(q).Encode(updateProfiles)
-
-}
-
-func deleteProfiles(q http.ResponseWriter, r *http.Request) {
-
-	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-
-		q.WriteHeader(400)
-		q.Write([]byte("Id could not be converted to integer"))
-		return
-	}
-	if id >= len(profiles) {
-		q.WriteHeader(404)
-		q.Write([]byte("no profile found with specific ID"))
-		return
-	}
-
-	profiles = append(profiles[:id], profiles[id+1:]...)
-	q.WriteHeader(200)
-
-}
-*/
 func main() {
 	initDB()
 	log.Println("Starting the HTTP server on port 8090")
